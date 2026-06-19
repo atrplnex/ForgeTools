8e587d1719662e2c4865754a5c7a693bf3c1ecc2
Jan Andrei De Mesa <jan.andrei.demesa@ntt.com.ph>
2026-06-19 08:50:03 +0800

    Changes 2

--- components/tools/WebpConverter/WebpConverter.tsx
+++ components/tools/WebpConverter/WebpConverter.tsx
@@ -4,7 +4,15 @@ import modals from "../ModalDesign/modal.module.css";
 export default function WebpConverter() {
     return (
         <div className={modals.modalBackground}>
-            
+            <div className={modals.modalTitle}>
+                <h2>WebP Converter</h2>
+            </div>
+            <div className={modals.modalDragDropContainer}>
+                <label>Drag Images
+                    <i className="fa-solid fa-image"></i>
+                    <input type="file" accept=".png,.jpg,.svg" multiple className="hidden"/>
+                </label>
+            </div>
         </div>
     );
 }
